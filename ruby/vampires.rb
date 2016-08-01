21a011d86d32815fe42ef1a87276ef6fff9a2fb3
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-01 13:17:35 -0400

    Add multiple employees feature

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,43 +1,39 @@
+puts "How many employees will be processed?"
+employees = gets.chomp
 
-#vampire survey
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp
-puts "What year were you born?"
-born = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
+employees.to_i.downto(1) do
+	#vampire survey
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp
+	puts "What year were you born?"
+	born = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic = gets.chomp
+	puts "Would you like to enroll in the company’s health insurance?"
+	insurance = gets.chomp
 
-#preferences
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
+	#Determine if age is correct
+	year = "2016".to_i
 
-year = "2016".to_i
+	if age.to_i == year - born.to_i
+		youth = true
+	else
+		youth = false
+	end
 
-#Determine if age is correct
-if age.to_i == year - born.to_i
-	youth = true
-else
-	youth = false
+	#Determine if likely a vampire
+
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire"
+	elsif youth == false && (garlic == "no" && insurance == "no")
+		puts "Almost certainly a vampire."
+	elsif youth == false && (garlic == "no" || insurance == "no")
+		puts "probably a vampire."
+	elsif youth != false && (garlic == "yes" || insurance == "yes")
+		puts "Probably not a vampire."
+	else 
+		puts "Results inconclusive"
+	end
 end
-
-#Determine if likely a vampire
-
-if name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire"
-elsif youth == false && (garlic == "no" && insurance == "no")
-	puts "Almost certainly a vampire."
-elsif youth == false && (garlic == "no" || insurance == "no")
-	puts "probably a vampire."
-elsif youth == true && (garlic == "yes" || insurance == "yes")
-	puts "Probably not a vampire."
-else 
-	puts "Results inconclusive"
-end
-
-

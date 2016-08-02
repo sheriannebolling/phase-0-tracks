b3be653da35e6274f344656c1db6a03789a856d6
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-02 18:36:31 -0400

    Add hamster data

---
+++ ruby/hamsters.rb
@@ -0,0 +1,15 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "How loud is the hamster's volume level on a scale of 1 through 10?"
+squeakiness = gets.chomp
+puts = "What is your hamster's fur color"
+fur = gets.chomp
+puts "Is the hamster a good candidate for adoption?"
+candidate = gets.chomp
+puts "What is the hamsters estimated age?"
+if gets.chomp == ""
+	age = nil
+else age = gets.chomp
+end
+
+

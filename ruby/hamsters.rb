93fc854d5fb9d885ba43026d55bed86d4ea39969
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-02 20:00:47 -0400

    Add indentation

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,11 +1,15 @@
 puts "What is the hamster's name?"
 name = gets.chomp
+
 puts "How loud is the hamster's volume level on a scale of 1 through 10?"
 squeakiness = gets.chomp.to_i
+
 puts "What is your hamster's fur color?"
 fur = gets.chomp
+
 puts "Is the hamster a good candidate for adoption?"
 candidate = gets.chomp
+
 puts "What is the hamsters estimated age?"
 age = gets.chomp.to_i
 
@@ -13,7 +17,6 @@ if age == nil
 	age = nil
 end
 	
-
 puts "Hamster Name: #{name}"
 puts "Volume Level: #{squeakiness}"
 puts "Fur Color: #{fur}"

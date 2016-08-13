a0bc0c59fc11bed8648ba8e697e1611c1b34f3a1
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-13 10:45:37 -0400

    Add Puppy Class and list both characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+=begin
+Class ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Leg count: 4
+Hair Type: fur
+Color: varies
+Name: varies
+Tail: varies
+Eyes: 2
+Ears: 2
+Size: varies
+
+BEHAVIOR -----------------------
+Sniff
+Lick
+Pounce
+Sleep
+Eat
+Yawn
+Bark
+=end

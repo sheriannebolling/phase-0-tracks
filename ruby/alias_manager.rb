c782b53b165962c1667acedfe8ed0b58234072c1
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-14 17:42:54 -0400

    Add method that adds names to hash

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,6 +4,8 @@ Method Output - string (fake name)
 Program Input - strings of first and last names
 Program Output - iterated hash listing string of real and fake names
 
+Create hash to hold real and fake names
+Create method to add real and fake names to hash
 Set variables for first and last names and make them lowercase
 Set variables for vowels and consonants
 Write method that takes names as arguments
@@ -14,21 +16,30 @@ In method swap the first and last name
 Return string with first letter uppercase
 Test "Felicia Torres" which should become "Vussit Gimodoe"
 Add user interface
-Create hash to hold real and fake names
-Iterate through hash printing real and fake names
+Iterate through hash printing real and fake names in if/else statement
 =end 
+
+
+#Create hash to hold real and fake names
+#Create method to add real and fake names to hash
+name_hash = {}
+
+def add_name_to_hash(real_name, fake_name, hash)
+	hash[real_name] = fake_name
+end
+
 
 #Method swaps first and last name
 #Changes vowels to next vowel
 #Changes consonants to next consonant
-
-def swap_names(input)
+def swap_names(input, hash)
+		new_hash = hash
 		altered_input = input
 		altered_input.downcase!
 		index = 0
 		transformed_input = ""
-		vowels = "aeiouya"
-		consonants = "bcdfghjklmnpqrstvwxzb"
+		vowels = "aeioua"
+		consonants = "bcdfghjklmnpqrstvwxyzb"
 		while index < altered_input.length
 			current_letter = altered_input[index]
 			if current_letter == " "
@@ -50,7 +61,7 @@ def swap_names(input)
 		name_array.reverse!
 		new_name = name_array.join(" ")
 		new_name.capitalize!
-		
+		add_name_to_hash(input, new_name, new_hash)
 end 
 
 #Test code - should return:  "Vussit gimodoe is actually Felicia torres"
@@ -62,27 +73,28 @@ puts "Would you like to get an alias name? Type yes to continue or type quit to 
 get_name = gets.chomp
 
 
-#Loop through user interface 
+#Loop through user interface and iterate through hash when ueser quits 
 while get_name == "yes"
 		puts "What is the first and last name you like to get an alias for?"
 		input = gets.chomp
-		swap_names(input)
-		name_hash = {}
-		name_hash.merge!(input => swap_names(input))
-		p swap_names(input)	
+		swap_names(input, name_hash)
+		p name_hash
 
 		puts "Would you like to get an alias name, type yes to continue or type quit to exit."
 		get_name = gets.chomp
 
 		if get_name == "quit"
-		puts "Ok no problem"
-		name_hash.each_pair do 
-			|input, new_name| puts "#{new_name}" + " " + "is actually" + " " + "#{input}!"
-		end	
+			puts "Ok no problem"
+			name_hash.each_pair do 
+				|input, new_name| puts "#{new_name}" + " " + "is actually" + " " + "#{input}!"
+			end	
 		elsif get_name == "yes"
-		get_name == "yes"
+			get_name == "yes"
 		else 
-		puts "I dont understand"
+			puts "I dont understand"
+			name_hash.each_pair do 
+				|input, new_name| puts "#{new_name}" + " " + "is actually" + " " + "#{input}!"
+			end	
 		end
 end
 
@@ -91,10 +103,6 @@ puts "Thank you for using the alias-master!"
 
 #notes
 =begin
-I was unable to get the hash to store multiple keys and values. It seems that as soon as the method
-runs a second time the key and value are saved over. I tried a few different ways to add keys and values but
-couldn't get it to work so this was as far as I was able to get it. 
-
 I was able to capitalize the first letters in the names when running the program but I know .capitalize
 only capitalizes the first letter in a string. I tried finding a way to capitalize them when they were split into two 
 arrays when I swapped the names but didn't succeed in finding a way. 

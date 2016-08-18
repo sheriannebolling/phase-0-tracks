808dd92c56a62d79df6dac895a996a0c14a6df0b
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-17 20:49:44 -0400

    Add truck class design

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+=begin
+Class ------------------------
+Truck
+
+Attributes ----------------
+Color: varies
+Size: varies
+Age: varies
+
+Methods -----------------------
+Honk
+Turn - takes argument direction
+Go fast - takes argument speed
+Stop
+=end

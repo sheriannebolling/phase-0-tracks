e567f74cfb7baa20944054f826fb093dee7b50b2
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-21 16:24:02 -0400

    Make horse object and added horses and their names to it

--- js/data_structures.js
+++ js/data_structures.js
@@ -2,7 +2,21 @@ var colorArray = ["blue", "green", "white", "black"];
 var horseArray = ["Whispers", "Johnny", "Carrots", "Richard"];
 
 horseArray.push("Ed");
-console.log(horseArray);
+//console.log(horseArray);
 
 colorArray.push("Magenta");
-console.log(colorArray);
+//console.log(colorArray);
+
+horseObject = {}
+
+function Horses(horseObject, horseArray, colorArray){
+	for(var i = 0; i < horseArray.length; i++){
+		horseObject[horseArray[i]] = colorArray[i];
+	};
+	console.log(horseObject);
+}
+
+Horses(horseObject, horseArray, colorArray);
+
+
+

9d07ea0f347084e7e40c3b8111cbcae71981530e
Sherianne Bolling <sheriannebolling@gmail.com>
2016-08-21 16:09:21 -0400

    Add color and horse arrays and push in new color and horse

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colorArray = ["blue", "green", "white", "black"];
+var horseArray = ["Whispers", "Johnny", "Carrots", "Richard"];
+
+horseArray.push("Ed");
+console.log(horseArray);
+
+colorArray.push("Magenta");
+console.log(colorArray);

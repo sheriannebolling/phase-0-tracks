31513d02551e104b138d6d8eabc4249e9c5c47a8
Sherianne Bolling <sheriannebolling@gmail.com>
2016-09-08 09:11:40 -0400

    Add class to pseudocode and rspec in red status

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,17 +1,37 @@
 
-# Input: Word given by user 
-# Create a variable to hold the word user-1 enters. 
-# Create a new variable holding an array of letter of the word user-1 enters.
-# Create another array of "_" that matches the length of the word.
-# Print that array on one line with spaces.
-# Create a variable to hold the guess user-2 enters - The variable should store the guesses
-# Create a variable that counts the number of guesses
-# Create a while loop that allows the user to guess until there are no guesses left.
-	# If the guess doesn’t match a previous guess the count goes down by one
-	# If the guess matches a previous guess the count does not go down.
-	# If guess matched any of the letters of the  word(using iteration) change the index in 
-		# the _ variable to that letter. 
-	# Print new version of array after each guess
-# If the new array is equal to the array of the word variable then display 
-	#congratulatory message if not a taunting message.
-# Output: Message congratulating or taunting
+
+
+# Outside class
+	# Input: Word given by user and guesses by second user
+	# Create a while loop that allows the user to guess until there are no guesses left.
+	# Output: Message congratulating or taunting
+
+# Create a class 
+	# Create a variable to hold the word user-1 enters. 
+	# Create a variable to hold the guess user-2 enters - The variable should store the guesses
+	# Create a variable that counts the number of guesses
+		# Create a method to create a new variable that holds an array of letters of the word user-1 enters.
+		# Create a method to create another array of "_" that matches the length of the word.
+		# Create a method to check if guess matches
+			# If guess matched any of the letters of the  word(using iteration) change the index in 
+			# the _ variable to that letter. 
+		# Create a method to count
+			# If the guess doesn’t match a previous guess the count goes down by one
+			# If the guess matches a previous guess the count does not go down.
+			# Prints to try again they have already guessed that letter
+		# Create a method to print 
+			# Print array on one line with spaces.
+			# Print new version of array after each guess
+		# Create a method to check if win
+			# If the new array is equal to the array of the word variable then display 
+			# congratulatory message if not a taunting message.
+			
+		
+
+	
+		
+		
+		
+
+
+

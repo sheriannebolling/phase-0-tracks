77bfb0731a2e1397129114dc54970bc054140162
Sherianne Bolling <sheriannebolling@gmail.com>
2016-09-23 12:45:30 -0400

    Add code to create database with feelings table, and insert each feeling into table

--- databases/something_awesome/motivationator.rb
+++ databases/something_awesome/motivationator.rb
@@ -1,31 +1,76 @@
-# pseudocode
-# input - feeling
-# output - quote
-# create a database
-# create a tracker table that stores feeling entered with date entered 
-# create a feelings table with a foreign key to tracker table
-# create a quotes table with a foreign key to feelings table
-# enter data into quotes table and feelings table
-# create user interface that asks how the user is feeling
-# if statement that prints the quote that relates to the feeling
+# Pseudocode
+# Require gems
+# Create Database
+# Create 4 tables - 1 for quotes, 2 for feelings, 3 for user info, 4 is main table to track
+# Create user interface to ask how feel today
+# Store the date, name, and feeling in table three
+# Retrieve quote that matched feeling entered and print to screen
 
-
-#require gem
+# require gems
 require 'sqlite3'
 
-# create SQLite3 database
-moti_db = SQLite3::Database.new("motivationator.db")
+# # create SQLite3 database
+motivationator_db = SQLite3::Database.new("motivationator.db")
 
-#create table
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS quotes(
+# # create_table_quotes = <<-SQL
+# #   CREATE TABLE IF NOT EXISTS quotes(
+# #     id INTEGER PRIMARY KEY,
+# #     quote VARCHAR(255),
+# #     feeling_id INT
+# #   )
+# # SQL
+
+create_table_feelings = <<-SQL
+  CREATE TABLE IF NOT EXISTS feelings(
     id INTEGER PRIMARY KEY,
-    quote VARCHAR(255),
-    
+    feeling_name VARCHAR(255)
   )
 SQL
 
-#User interface
-puts "How are you feeling today? Please choose one of the following: frustrated, sad, tired, angry, uninspired, or defeated"
-feeling = gets.chomp
+# # create_table_tracker = <<-SQL
+# #   CREATE TABLE IF NOT EXISTS tracker(
+# #     id INTEGER PRIMARY KEY,
+# #     feeling_id INT,
+# #     quote_id INT,
+# #     enter_date DATE,
+# #     user_id INT
+# #   )
+# # SQL
+
+# # create_table_user = <<-SQL
+# #   CREATE TABLE IF NOT EXISTS user(
+# #     id INTEGER PRIMARY KEY,
+# #     first_name VARCHAR(255),
+# #     last_name VARCHAR(255)
+# #   )
+# # SQL
+
+
+motivationator_db.execute(create_table_feelings)
+# # motivationator_db.execute(create_table_tracker)
+# # motivationator_db.execute(create_table_user)
+# # motivationator_db.execute(create_table_quotes)
+
+# # def create_quote(db, quote, feeling_id)
+# #   db.execute("INSERT INTO quotes (quote, feeling_id) VALUES (?, ?)", [quote, feeling_id])
+# # end
+
+
+
+#Create feeling database and enter feelings
+=begin
+def create_feeling(db, feeling_name)
+  db.execute("INSERT INTO feelings (feeling_name) VALUES (?)" , [feeling_name])
+end
+
+feelings = ["frustrated", "sad", "tired", "angry", "uninspired", "defeated"]
+
+
+feelings.length.times do |feeling|
+	create_feeling(motivationator_db, feelings[feeling])
+end
+
+
+
+
 
